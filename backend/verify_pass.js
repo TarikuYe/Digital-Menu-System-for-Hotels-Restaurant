174c64ef04155f607a5330e7bd2f4a2dd75efcea
unknown <tarikuneg911@gmail.com>
2026-07-29 10:42:12 +0300

    Remove unwanted files and test files

--- backend/verify_pass.js
+++
@@ -1,8 +0,0 @@
-import bcrypt from 'bcryptjs';
-
-const hash = '$2b$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy';
-const password = 'admin123';
-
-bcrypt.compare(password, hash).then(res => {
-    console.log('Password match:', res);
-});
